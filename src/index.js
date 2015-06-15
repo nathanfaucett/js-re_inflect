e611e51432303d9c6ce301e54381e5eb035ee2cf
Nathan Faucett <nathanfaucett@gmail.com>
2015-06-15 12:34:11 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-module.exports = /[^A-Z-_ \.]+|[A-Z][^A-Z-_ \.]+|[^a-z-_ \.]+/g;
+module.exports = /[^A-Z-_ ]+|[A-Z][^A-Z-_ ]+|[^a-z-_ ]+/g;
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var assert = require("assert"),
 
 describe("reInflect", function() {
     it("should split string", function() {
-        var parts = "asdf_asdf-asdf asdf".match(reInflect);
-        assert.deepEqual(parts, ["asdf", "asdf", "asdf", "asdf"]);
+        var parts = "asdf_asdf-asdfAsdf asdf.asdf".match(reInflect);
+        assert.deepEqual(parts, ["asdf", "asdf", "asdf", "Asdf", "asdf.asdf"]);
     });
 });
